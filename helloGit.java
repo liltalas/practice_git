62543b5dc5cba2d0e2ddd6e83d051f644a59aa6c
liltalas <zamirbekovasyrga@gmail.com>
2022-11-06 21:29:56 -0600

    added favorite city

--- helloGit.java
+++ helloGit.java
@@ -1,5 +1,20 @@
+import java.util.Objects;
+import java.util.Scanner;
+
 public class helloGit {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
         System.out.println("Hello Git!");
+
+        System.out.println("What is your favorite city? ");
+        String city = sc.next();
+
+        if(Objects.equals(city, "Boston")){
+            System.out.println("Awesome :) ");
+        } else{
+            System.out.println("Cool XD");
+        }
+
     }
 }
